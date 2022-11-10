c39397e63ff34e006207b46c782ad8a8c92680be
Fernando <you@example.com>
2022-11-10 10:57:56 +0100

    Style(Colors.js): Desarrollada la constante con los colores principales que usaremos en el proyecto

---
+++ themes/Colors.js
@@ -0,0 +1,11 @@
+
+const colors = {
+    primaryBackgroundLight : '#00A86A',
+    primaryBackgroundDark : '#00783F',
+
+    secundaryBackgroundLight : '#78909C',
+    secundaryBackgroundDark : '#4A626D',
+
+    primaryTextColor : '#fff'
+    
+}
