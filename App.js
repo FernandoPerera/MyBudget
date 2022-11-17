c53b69a7d37db513921ab39b261ab964fb070884
Fernando <you@example.com>
2022-11-17 10:30:47 +0100

    Style(App.js): Realizado el diseño de los inputs y de los botones de añadir y mostrar lista

--- App.js
+++ App.js
@@ -1,22 +1,90 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { useState } from 'react'
+import { StyleSheet, Text, TextInput, View, KeyboardAvoidingView,
+        TouchableWithoutFeedback, Keyboard, Platform, Pressable} from 'react-native'
 
 import { pallete } from './themes/Colors'
 
 export default function App() {
+
+  const [show, setShow] = useState(true)
+
+  const hideElementsForKeyboard = () => {
+    setShow(!show)
+    Keyboard.dismiss()
+  }
+
   return (
     <View style={styles.container}>
-      
-      <View style={styles.balanceContainer}>
 
-        <Text>Balance Container</Text>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          style={{flex: 1, width: '100%'}}
+          keyboardVerticalOffset={Platform.OS === "ios" ? -20 : -80}>
+          
+            <View style={styles.balanceContainer}>
 
-      </View>
+              <Text>Balance Container</Text>
 
-      <View style={styles.inputContainer}>
+            </View>
 
-        <Text>Input Container</Text>
+            <View style={styles.inputContainer}>
 
-      </View>
+              <View style={styles.datesContainer}>
+
+              <View style={styles.listbuttom}>
+
+                
+                {show ? <Pressable style={styles.pressableToList}>
+
+                          <Text>Mostrar lista</Text>
+
+                        </Pressable>
+                      : null
+                }
+
+              </View>
+
+                <View style={{flexDirection:'row'}}>
+
+                  <TextInput style={styles.rowInputs}
+                  placeholder='Movimiento...'
+                  onPressIn={() => !show ? null : setShow(!show)}
+                  onSubmitEditing={hideElementsForKeyboard}/>
+
+                  <TextInput style={styles.rowInputs}
+                  placeholder='Fecha...'
+                  onPressIn={() => !show ? null : setShow(!show)}
+                  onSubmitEditing={hideElementsForKeyboard}/>
+
+                </View>
+
+                <View style={{flexDirection:'row'}}>
+
+                <TextInput style={styles.descriptionInput}
+                  placeholder='Descripcion...'
+                  textAlign='left'
+                  maxLength={40}
+                  multiline={true}
+                  onPressIn={() => !show ? null : setShow(!show)}
+                  onSubmitEditing={hideElementsForKeyboard}/>
+
+                <Pressable onPress={ !show ? hideElementsForKeyboard : null}>
+
+                  <View style={styles.pressableToAdd}>
+
+                    <Text>Añadir</Text>
+
+                  </View>
+
+                </Pressable>
+
+                </View>
+
+              </View>
+
+            </View>
+          </KeyboardAvoidingView>
+
       
     </View>
   );
@@ -41,6 +109,47 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  datesContainer: {
+    flex: 2,
+    backgroundColor: pallete.primaryBackgroundLight,
+    width: '75%',
+    borderRadius: 15,
+    marginVertical: 50,
+  },
+  rowInputs: {
+    flex: 1,
+    padding: 10,
+    marginHorizontal: 15,
+    borderRadius:12,
+    marginVertical: 20,
+    backgroundColor: pallete.secundaryBackgroundLight,
+  },
+  descriptionInput: { 
+    backgroundColor: pallete.secundaryBackgroundLight,
+    borderRadius: 12,
+    padding: 10,
+    width: 150,
+    height: 70,
+    marginLeft: 15,
+    marginBottom: 65
+  },
+  listbuttom: {
+    flex: 1, 
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  pressableToList: {
+    backgroundColor: pallete.primaryBackgroundDark,
+    borderRadius: 12,
+    padding: 20,
+  },
+  pressableToAdd: {
+    marginTop: 10,
+    marginLeft: 23,
+    backgroundColor:pallete.primaryBackgroundDark,
+    borderRadius: 12,
+    padding: 15
   }
 
 });
